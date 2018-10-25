21a4fb8e0d1cd23c758be05093ab0297fbecf63b
Yongqi Guo 郭永奇 <G10002198@b-and-qchina.com>
2018-10-25 11:05:15 +0800

    'IE适配'

---
+++ README.md
@@ -0,0 +1,5 @@
+环境部署： <br>
+本地开发环境： npm start <br>
+线上开发环境： npm run dev <br>
+线上测试环境： npm run uat <br>
+线上生产环境： npm run prod <br>
--- public/reset/IE.js
+++ public/reset/IE.js
@@ -1,3 +1,8 @@
 if(!window.location.origin) {
 	window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
 }
+
+window.console = window.console || (function(){ 
+	var c = {}; c.log = c.warn = c.debug = c.info = c.error = c.time = c.dir = c.profile = c.clear = c.exception = c.trace = c.assert = function(){}; 
+	return c; 
+})();
