6c046d1bdf63b74a97294fb9f282eb453cb75841
Yongqi Guo 郭永奇 <G10002198@b-and-qchina.com>
2019-02-26 14:37:14 +0800

    '修改配置'

--- config/index.js
+++ config/index.js
@@ -1,14 +1,14 @@
 const url = {
 	development: {
 		targetUrl: 'http://customer.dev-zt.bnq.com.cn',
-		apiUrl: 'http://localhost:8088',
+		apiUrl: 'http://web.futureshop.dev-zt.bnq.com.cn:8088',
 		apiUrlFilter: '/customerAdmin',
 		authUrl: 'http://auth.dev-zt.bnq.com.cn',
 		authUrlFilter: '/auth',
 		port: 8088,
 		autoOpenBrowser: true, 
 		proxyFilter: '/customerAdmin',
-        addressUrl:'http://customer.dev-zt.bnq.com.cn/areas/district',
+        addressUrl:'http://web.futureshop.dev-zt.bnq.com.cn:8088/areas/district',
 	},
 	prodDev: {
 		apiUrl: 'http://192.168.200.54:8080/',
